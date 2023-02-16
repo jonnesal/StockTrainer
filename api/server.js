697307ee3f5a534ffe51d51aca49840f12ff27b1
jonnesal <Jonne.Salli@metropolia.fi>
2023-02-16 22:27:11 +0200

    express toimii

--- api/server.js
+++ api/server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+
+
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Fetched from: {{ mountain }}</h1>
   </div>
 </template>
+
+<script setup>
+const { data: mountain } = await useFetch('/http://localhost:4001')
+console.log(mountain);
+</script>
