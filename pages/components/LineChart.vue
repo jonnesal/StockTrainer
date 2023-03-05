fba4e1a50b32ceff7aef9fa3b7ff3fa1c16d6ae7
jonnesal <Jonne.Salli@metropolia.fi>
2023-03-06 01:21:56 +0200

    list testiin

--- pages/components/LineChart.vue
+++ pages/components/LineChart.vue
@@ -1,88 +1,109 @@
 <template>
-  <div>
-    <LineChart
-      :chart-data="data"
-      :options="options"
-      css-classes="chart-container"
-    />
-    <button @click="updateChartData" class="my-button">Update Chart Data</button>
-    <p> Data API Kautta </p>{{ users }}
+  <div class="chart-wrapper">
+    <div class="chart-container">
+      <LineChart :chart-data="data" :options="options" />
+    </div>
+    <div class="controls">
+      <button @click="updateData" class="btn">Update Data</button>
+      <ul class="lines">
+        <li v-for="(value, index) in dataValues" :key="index">{{ value }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { LineChart } from "vue-chart-3"
+import { LineChart } from 'vue-chart-3';
 import {
   Chart,
   LineController,
   CategoryScale,
   LinearScale,
   PointElement,
-  LineElement
-} from "chart.js"
+  LineElement,
+} from 'chart.js';
+
 Chart.register(
   LineController,
   CategoryScale,
   LinearScale,
   PointElement,
   LineElement
-)
-const dataValues = ref([12, 14, 13, 10, 11, 13, 15])
+);
+
+const dataValues = ref([12, 14, 16, 18, 11, 13, 15]);
 const data = computed(() => ({
-  labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
   datasets: [
     {
-      label: "Foo",
+      label: 'Foo',
       data: dataValues.value,
-      backgroundColor: "#dc322f",
-      borderColor: "#ffffff"
-    }
-  ]
-}))
+      borderColor: '#fff',
+      backgroundColor: '#dc322f',
+    },
+  ],
+}));
 const options = ref({
   plugins: {
     title: {
-      text: "Line"
-    }
-  }
-})
+      text: 'Line',
+      color: '#fff',
+    },
+  },
+});
 
-function updateChartData() {
-  dataValues.value = [
-    getRandomInt(10, 20),
-    getRandomInt(10, 20),
-    getRandomInt(10, 20),
-    getRandomInt(10, 20),
-    getRandomInt(10, 20),
-    getRandomInt(10, 20),
-    getRandomInt(10, 20)
-  ]
-}
+const addLine = (value) => {
+  const list = document.querySelector('.lines');
+  const line = document.createElement('li');
+  line.textContent = value;
+  list.appendChild(line);
+};
 
-const { data: users } = await useAsyncData('count', () => $fetch('http://localhost:3001/testi/users'))
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
+const updateData = () => {
+  dataValues.value = dataValues.value.map((value) => value + Math.floor(Math.random() * 10));
+  addLine(dataValues.value[dataValues.value.length - 1]);
+};
 
 </script>
 
 <style scoped>
-.my-button {
-  border-radius: 8px;
-  background-color: black;
-  color: white;
-  padding: 12px 24px;
+.chart-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-color: #333;
+}
+
+.chart-container {
+  flex: 1;
+  max-width: 700px;
+}
+
+.controls {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.btn {
+  margin-bottom: 10px;
+  background-color: #fff;
+  color: #333;
+  border: none;
+  border-radius: 10px;
+  padding: 5px 10px;
   cursor: pointer;
 }
 
-.my-button:hover {
-  background-color: white;
-  color: #0074D9;
+.lines {
+  list-style: none;
+  margin: 0;
+  padding: 0;
 }
 
-.my-button:focus {
-  outline: none;
-  box-shadow: 0 0 0 2px white;
+.lines li {
+  color: #fff;
+  margin-bottom: 5px;
 }
 </style>
